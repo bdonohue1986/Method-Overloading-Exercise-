0881ed5e32c1751aeda36004de180515684839bc
bdonohue1986 <100311620+bdonohue1986@users.noreply.github.com>
2022-10-12 11:47:55 -0400

    another one done

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,46 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static double Add(double num1, double num2)
         {
-            Console.WriteLine("Hello, World!");
+            return num1 + num2;
+        }
+
+        public static double Add(double num1, double num2, double num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        public static int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(double num1, double num2, bool rightOn)
+        {
+            var sum = num1 + num2;
+             
+            if (rightOn == true && sum >= 0)
+            {
+                return $" ${sum} Dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+        static void Main(string[]args)
+        {
+            var x = 78.9;
+            var y = 10.76;
+
+            var answer = Add(x, y, true);
+
+            Console.WriteLine($"when adding {x} and {y} you will come out with {answer}");
         }
     }
 }
